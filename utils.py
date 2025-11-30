a5c827e8e23bcfc7c0a11c5df9654842aaf2d436
manavnagpal08 <72203264+manavnagpal08@users.noreply.github.com>
2025-11-30 22:09:37 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,9 @@
+import uuid
+from datetime import datetime
+
+def generate_order_id():
+    return "SPP" + datetime.now().strftime("%Y%m%d%H%M%S")
+
+def today():
+    return datetime.now().strftime("%d-%m-%Y")
 
